669bb42431b1b1ef9eb08c87d203e56a3c9309b2
kMed <kenta.medina@protonmail.com>
2024-03-06 11:22:54 +0900

    inventory in progress, basic functionality
    - objects are stored in the "private List<GameObject> inventory" when picked up using the left mouse button

--- SGJ9-Team5-Game/Assets/FpvInteractController.cs
+++ SGJ9-Team5-Game/Assets/FpvInteractController.cs
@@ -19,7 +19,10 @@ public class FpvInteractController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            InteractWith();
+        }
     }
 
     void InteractWith()
